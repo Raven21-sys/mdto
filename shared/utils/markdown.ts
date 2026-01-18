795dd8f03be995d99192db89da9d373383e49530
hhhjin <kkp7125@gmail.com>
2026-01-18 13:34:56 +0900

    feat: add gfm support

--- shared/utils/markdown.ts
+++ shared/utils/markdown.ts
@@ -3,6 +3,7 @@ import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -15,6 +16,7 @@ import { unified } from "unified";
 export async function markdownToHtml(markdown: string): Promise<string> {
 	const processor = unified()
 		.use(remarkParse)
+		.use(remarkGfm)
 		.use(remarkRehype, { allowDangerousHtml: true })
 		.use(rehypeRaw)
 		.use(rehypeSanitize)
