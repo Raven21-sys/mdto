5fab3e6f771e8bf658f7b2376abb1f417e9b7593
hhhjin <kkp7125@gmail.com>
2026-01-27 21:23:00 +0900

    fix: remove some env vars

--- server/config/env.ts
+++ server/config/env.ts
@@ -2,15 +2,11 @@ import { z } from "zod";
 
 const envSchema = z
 	.object({
-		ENV: z.string().min(1),
 		TURNSTILE_SECRET_KEY: z.string().min(1),
 		BETTER_AUTH_SECRET: z.string().min(1),
 		BETTER_AUTH_URL: z.string().min(1),
 		GITHUB_CLIENT_ID: z.string().min(1),
 		GITHUB_CLIENT_SECRET: z.string().min(1),
-		ASSETS: z.unknown(),
-		BUCKET: z.unknown(),
-		DB: z.unknown(),
 	})
 	.loose();
 
